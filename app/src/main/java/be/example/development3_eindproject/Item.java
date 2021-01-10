53ce156086c2771631fd2b5d41ff46a24037db6b
Frauke <frauke_heedfeld@hotmail.com>
2021-01-10 20:15:51 +0100

    Start - before adjusting files2

---
+++ app/src/main/java/be/example/development3_eindproject/Item.java
@@ -0,0 +1,10 @@
+package be.example.development3_eindproject;
+
+public class Item {
+    int id;
+    String title;
+
+    public String getTitle(){
+        return title;
+    }
+}
--- app/src/main/java/be/example/development3_eindproject/MainActivity.java
+++ app/src/main/java/be/example/development3_eindproject/MainActivity.java
@@ -2,9 +2,12 @@ package be.example.development3_eindproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -19,9 +22,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextViewResult;
+
+    static final String JsonUrlStr = "https://my.api.mockaroo.com/portfolioitems.json?key=7b107dd0";
+
+    LinearLayout rootLayout;
     //
     private RequestQueue mQueue;
 
@@ -30,57 +39,75 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextViewResult = findViewById(R.id.text_view_result);
-        Button buttonParse = findViewById(R.id.button_parse);
-        // Link naar button
+        rootLayout = findViewById(R.id.list_view);
 
-        mQueue = Volley.newRequestQueue(this);
+        try {
+            URL url = new URL(JsonUrlStr);
 
-        buttonParse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                jsonParse();
-                // Als er wordt geklikt, gaat onderstaande functie van start
+            FetchPortfolio getDataTask = new FetchPortfolio();
+            getDataTask.execute(url);
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class FetchPortfolio extends AsyncTask<URL, Void, Item[]> {
+        final String TAG = FetchPortfolio.class.getSimpleName();
+
+        URL url;
+
+        @Override
+        protected Item[] doInBackground(URL... params) {
+            url = params[0];
+
+            if(url.toString().contains(.json)){
+                return FetchPortfolioAsJSON();
             }
-        });
+            return new Item[0];
+        }
+
+        @Override
+        protected void onPostExecute(Item[] items) {
+            for (Item item: items) {
+
+                View portfolioView = getLayoutInflater().inflate(R.layout.list_item, null);
+
+                TextView titleView = portfolioView.findViewById(R.id.title);
+                titleView.setText(String.format("%s", item.getTitle()));
+
+                rootLayout.addView(portfolioView);
+            }
+        }
+
     }
 
     private void jsonParse() {
 
         String url = "https://my.api.mockaroo.com/portfolioitems.json?key=7b107dd0";
 
-        StringRequest request = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
                     @Override
-                    public void onResponse(String response) {
-                        // Eerste response wordt getoond als request mogelijk is
+                    public void onResponse(JSONArray response) {
+                        try {
 
-                        mTextViewResult.setText("API succesvol ingeladen :)))))");
-                        //mTextViewResult.setText("Dit is een tweede regel tekst");
-                        parseData(response);
+                            for(int i = 0; i< response.length();i++){
+                                JSONObject portfolio = response.getJSONObject(i);
+
+                                String title = portfolio.getString("title");
+                                // String omschrijving = portfolio.getString("description");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                mTextViewResult.setText("An error occured");
+                error.printStackTrace();
                 // Tweede wordt getoond als er een error is
             }
         });
-        mQueue.add(request);
-        super.onStart();
-    }
-
-    private void parseData(String response) {
-        try {
-            // Create JSOn Object
-            JSONArray jsonArray = new JSONArray(response);
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                mTextViewResult.setText(jsonObject.getString("id"));
-                //mTextViewResult.setText("En nu enkel nog de juiste data fixen...");
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
     }
 }
